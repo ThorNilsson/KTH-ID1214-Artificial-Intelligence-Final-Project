1c5847fd64c91869489db9d439a8501fb9c6265f
ThorNilsson <thor756nilsson@gmail.com>
2024-11-25 16:14:59 +0100

    Add environment configuration and enhance screenshot handling in scripts

--- getCoordinatesFromLog.sh
+++ getCoordinatesFromLog.sh
@@ -1,1 +1,3 @@
-tail -50  $LOCALAPPDATA/../Roaming/.minecraft/logs/latest.log | grep "The nearest" | tail -1 | sed 's/^.*The nearest //' | cut -d "[" -f2 | cut -d "]" -f1 | sed 's/~/256/g' | sed 's/,//g' > "coords.txt"
+MINECRAFT_LOG_SOURCE=$(cat .env | grep MINECRAFT_LOG_SOURCE= | sed 's/^.*=//')
+
+tail -50  $MINECRAFT_LOG_SOURCE | grep "The nearest" | tail -1 | sed 's/^.*The nearest //' | cut -d "[" -f2 | cut -d "]" -f1 | sed 's/~/256/g' | sed 's/,//g' > "coords.txt"
--- moveScreenshot.sh
+++ moveScreenshot.sh
@@ -1,16 +1,21 @@
-#Not really working, but the idea is to move the last screenshot to the training data folder
-#structure="monument"
+#to run this script, use the following command:
+# bash moveScreenshot.sh <structure>
+# where <structure> is the name of the structure that the screenshot is of
+# This script moves the most recent screenshot from the source directory to the destination directory
+# The source directory is specified in the .env file
+# The destination directory is the TrainingData directory, with a subdirectory for the structure specified
+# The file is renamed to the current date and time
+
 structure=$1
-#sourcePath="C:\Users\thor7\OneDrive\Bilder\Skärmbilder"
-sourcePath="C:\Users\thor7\Pictures\Screenshots"
+sourcePath=$(cat .env | grep SCREENSHOT_SOURCE_DIR= | sed 's/^.*=//')
 sourceFile=$(ls $sourcePath | grep .png | tail -1)
 
 destinationPath=$(echo "./TrainingData/$structure") 
-destinationFile=$structure_$(date +%Y-%m-%d_%H%M%S).png # Not used yet
+destinationFile=${structure}_$(date +%Y-%m-%d_%H%M%S).png
 
 echo $sourceFile
 echo $sourcePath
 echo $destinationPath
 
 mkdir --parents $destinationPath
-mv "$sourcePath\\$sourceFile" "$destinationPath/$sourceFile"
+mv "$sourcePath\\$sourceFile" "$destinationPath/$destinationFile"
