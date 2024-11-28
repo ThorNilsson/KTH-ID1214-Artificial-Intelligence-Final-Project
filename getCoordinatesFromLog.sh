02d10d093b11d033e27f541bfa74964f5f87af23
ThorNilsson <thor756nilsson@gmail.com>
2024-11-28 14:39:06 +0100

    Update coordinate extraction in getCoordinatesFromLog.sh to adjust height and append default values

--- getCoordinatesFromLog.sh
+++ getCoordinatesFromLog.sh
@@ -5,4 +5,4 @@
 
 MINECRAFT_LOG_SOURCE=$(cat .env | grep MINECRAFT_LOG_SOURCE= | sed 's/^.*=//')
 
-tail -50  $MINECRAFT_LOG_SOURCE | grep "The nearest" | tail -1 | sed 's/^.*The nearest //' | cut -d "[" -f2 | cut -d "]" -f1 | sed 's/~/256/g' | sed 's/,//g' > "coords.txt"
+tail -50  $MINECRAFT_LOG_SOURCE | grep "The nearest" | tail -1 | sed 's/^.*The nearest //' | cut -d "[" -f2 | cut -d "]" -f1 | sed 's/~/200/g' | sed 's/,//g' | sed 's/$/ 0 0 /'> "coords.txt"
